70c8afd30d7aaa44224469578e910933639066db
mac-ricoland <ricoland@macbook-pro.lan>
2022-11-03 14:12:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="form">
+        <h1>Create account</h1>
+        <div>
+          <p>Name</p>
+          <form action="">
+            <input type="text" placeholder="Jean Dupont" />
+            <p>Email</p>
+            <input type="text" placeholder="Jeandupont@lereacteur.io" />
+            <p>Password</p>
+            <input type="password" placeholder="JHrETOPIzzsdEE" />
+            <p>Confirm your password</p>
+            <input type="password" placeholder="JHrETOPIzzsdEE" />
+          </form>
+        </div>
+        <div>
+          <button className="button">Register</button>
+        </div>
+      </div>
+      <footer>
+        <p>Made with React at Le Reacteur by Rico</p>
+      </footer>
     </div>
   );
 }
